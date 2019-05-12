d2252392f07cd24b332db3acadd4af54838512e8
Elaiya Raja E <elaiyarajae@ELAIYA.local>
2019-05-12 13:19:09 +0530

    refactor: updated with semantic ui styles

--- src/components/Loding/Loding.js
+++ src/components/Loding/Loding.js
@@ -1,3 +1,20 @@
+/**
+ * copyright 2019 (C) ELAIYA
+ * 
+ * created on: May 12, 2019
+ * @author: Elaiya Raja E
+ * 
+ * ---------------------------------------------------------
+ * Revision History (Release 1.0.0)
+ * ---------------------------------------------------------
+ * VERSION | AUTHOR - DATE       | DESCRIPTION OF CHANGE
+ * --------------------------------------------------------- 
+ * 1.0     | ELAIYA - 12-05-2019 | Inital Creation
+ * ---------------------------------------------------------
+ * 
+ */
+
+//Library Files
 import React from 'react';
 
 const Loading = props => {
